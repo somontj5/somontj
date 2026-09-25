7e8574e1cc2cf305115e1e67fa6821ea66342ad5
somontj5 <elevenlabs9989@gmail.com>
2026-09-25 10:16:00 +0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -42,7 +42,7 @@ KEEPALIVE_PORT = int(os.environ.get("PORT", "0") or "0")
 
 SEARCH_URL = os.environ.get(
     "SOMON_URL",
-    "https://m.somon.tj/telefonyi-i-svyaz/mobilnyie-telefonyi/sostoyanie---1/?ordering=newest&location=185,187,195,204,205,230,210,180"
+    "https://m.somon.tj/telefonyi-i-svyaz/mobilnyie-telefonyi/"
 )
 MAX_NEW_ITEMS_PER_RUN = int(os.environ.get("MAX_NEW_ITEMS_PER_RUN", "5"))
 MAX_SOLD_CHECKS_PER_RUN = int(os.environ.get("MAX_SOLD_CHECKS_PER_RUN", "5"))
